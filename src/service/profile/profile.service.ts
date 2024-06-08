6c1dce490945ef109143c04cb0e63ee7f4767d50
Dheeraj <137279485+dheerajt112@users.noreply.github.com>
2024-06-08 16:01:14 +0530

    cart-funtionality

--- src/service/profile/profile.service.ts
+++ src/service/profile/profile.service.ts
@@ -1,10 +1,12 @@
 import { UnauthorizedException } from "../../utils/errors";
 
+export const getProfile = async (
+  _: any,
+  input: { userId: string },
+  context: Record<string, any>
+) => {
+  console.log(context.auth, context.user);
 
-export const getProfile = async (_: any, input: { userId: string }, context: Record<string, any>) => {
-
-    console.log(context.auth,context.user);
-
-    if (!context.auth) throw new UnauthorizedException("please login")
-    return { status: 200, data: context.user }
+  if (!context.auth) throw new UnauthorizedException("please login");
+  return { status: 200, data: context.user };
 };
